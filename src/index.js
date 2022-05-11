3a8835edda9f4a031ed1d31e1edc6d5e0f099812
camilevahviraki <camilevahviraki@gmail.com>
2022-05-11 09:33:33 +0200

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
 import './styles/style.css';
+import $ from 'jquery';
 import wrapContainer from './modules/WrapContainer.js';
 import wrapscores from './modules/wrapScores.js';
 
 wrapContainer();
 
-wrapscores();
+// ZEIiDb0Iy2wNzVD068jm
+
+const inputName = document.getElementById('name');
+const inputScore = document.getElementById('scoreIn');
+const submitBtn = document.getElementById('Submit');
+const refreshBtn = document.getElementById('refresh');
+
+submitBtn.addEventListener('click', () => {
+  const score = {
+    user: inputName.value,
+    score: inputScore.value,
+  };
+  $.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ZEIiDb0Iy2wNzVD068jm/scores/', score, (data, status) => {
+    console.log(data);
+  }).catch((error) => {
+    console.log(error);
+  });
+});
+
+refreshBtn.addEventListener('click', () => {
+  $.get('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ZEIiDb0Iy2wNzVD068jm/scores/', (response) => {
+    console.log(response.result);
+    const arr = response.result;
+
+    wrapscores(arr);
+  }).catch((error) => {
+    console.log(error);
+  });
+});
+
+$.get('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ZEIiDb0Iy2wNzVD068jm/scores/', (response) => {
+  console.log(response.result);
+  const arr = response.result;
+
+  wrapscores(arr);
+}).catch((error) => {
+  console.log(error);
+});
